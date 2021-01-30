74701f9cde7e22e269700e75de614d6f3566bfd8
Michese <michese48@mail.ru>
2021-01-30 17:51:24 +0300

    6

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
 import './style.css';
 import Car from './car/car'
+import {Component} from 'react'
 
-function App() {
-    const colorStyle = {
-        color: 'red',
-        fontSize: 34
+class App extends Component {
+
+    state = {
+        cars: [
+            {
+                title: 'car1',
+                description: 'Lorem ipsum dolor sit amet.',
+                year: 2020
+            },
+            {
+                title: 'car2',
+                description: 'Lorem ipsum dolor sit amet.',
+                year: 2021
+            },
+            {
+                title: 'car3',
+                description: 'Lorem ipsum dolor sit amet.',
+                year: 2016
+            }
+        ],
+        pageTitle: 'Hello, world!'
     }
 
-    const props = [
-        {
-            title: 'car1',
-            description: 'Lorem ipsum dolor sit amet.',
-            year: 2020
-        },
-        {
-            title: 'car2',
-            description: 'Lorem ipsum dolor sit amet.',
-            year: 2021
-        },
-        {
-            title: 'car3',
-            description: 'Lorem ipsum dolor sit amet.',
-            year: 2016
-        }
-    ]
-    return (
-        <div className="App">
-            <h1>Hello, world!</h1>
-            <div className={"content"}>
-                <Car title={props[0].title} description={props[0].description} year={props[0].year}>
-                    <p style={ colorStyle }>COLOR</p>
-                </Car>
-                <Car title={props[1].title} description={props[1].description} year={props[1].year}/>
-                <Car title={props[2].title} description={props[2].description} year={props[2].year}/>
+    render() {
+        const cars = this.state.cars;
+        return (
+            <div className="App">
+                <h1>{ this.state.pageTitle }</h1>
+                <div className={"content"}>
+                    <Car title={cars[0].title} description={cars[0].description} year={cars[0].year}/>
+                    <Car title={cars[1].title} description={cars[1].description} year={cars[1].year}/>
+                    <Car title={cars[2].title} description={cars[2].description} year={cars[2].year}/>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
