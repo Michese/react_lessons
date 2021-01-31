34e5302b55da49ec6d135abd589b55cb47f14181
Michese <michese48@mail.ru>
2021-01-31 14:13:19 +0300

    7

--- src/App.js
+++ src/App.js
@@ -4,36 +4,83 @@ import {Component} from 'react'
 
 class App extends Component {
 
-    state = {
-        cars: [
-            {
-                title: 'car1',
-                description: 'Lorem ipsum dolor sit amet.',
-                year: 2020
-            },
-            {
-                title: 'car2',
-                description: 'Lorem ipsum dolor sit amet.',
-                year: 2021
-            },
-            {
-                title: 'car3',
-                description: 'Lorem ipsum dolor sit amet.',
-                year: 2016
-            }
-        ],
-        pageTitle: 'Hello, world!'
+    constructor(props) {
+        super(props);
+        this.state = {
+            cars: [
+                {
+                    title: 'car1',
+                    description: 'Lorem ipsum dolor sit amet.',
+                    year: 2020
+                },
+                {
+                    title: 'car2',
+                    description: 'Lorem ipsum dolor sit amet.',
+                    year: 2021
+                },
+                {
+                    title: 'car3',
+                    description: 'Lorem ipsum dolor sit amet.',
+                    year: 2016
+                }
+            ],
+            pageTitle: 'Hello, world!',
+            showCars: false
+        }
+
+    }
+
+    onDeleteCarHandler = index => {
+        const cars = [...this.state.cars]
+        cars.splice(index, 1)
+        this.setState({
+            cars
+        })
+    }
+
+    onToggleCarsHandler = () => {
+        this.setState({
+            showCars: !this.state.showCars
+        })
+    }
+
+    onChangeCarTitleHandler = (newTitle, index) => {
+        console.log(newTitle, index)
+        const cars = [...this.state.cars]
+        cars[0].title = newTitle
+        this.setState({
+            cars
+        })
+    }
+
+    changeInput = event => {
+        this.setState({
+            pageTitle: event.target.value
+        })
     }
 
     render() {
-        const cars = this.state.cars;
+        console.log('Render');
+        let cars = null;
+        if (this.state.showCars) {
+            cars = this.state.cars.map((car, index) => {
+                return (<Car
+                    key={index}
+                    title={car.title}
+                    description={car.description}
+                    year={car.year}
+                    onChangeCarTitleHandler={event => this.onChangeCarTitleHandler(event.target.value, index)}
+                    onDeleteCarHandler={this.onDeleteCarHandler.bind(this, index)}
+                />)
+            })
+        }
+
         return (
             <div className="App">
                 <h1>{ this.state.pageTitle }</h1>
+                <button onClick={this.onToggleCarsHandler}>Click me!</button>
                 <div className={"content"}>
-                    <Car title={cars[0].title} description={cars[0].description} year={cars[0].year}/>
-                    <Car title={cars[1].title} description={cars[1].description} year={cars[1].year}/>
-                    <Car title={cars[2].title} description={cars[2].description} year={cars[2].year}/>
+                    { cars }
                 </div>
             </div>
         );
--- src/car/car.js
+++ src/car/car.js
@@ -1,9 +1,10 @@
-const Car = props => (
+let Car = props => (
     <div className="car">
         <h3>{props.title}</h3>
         <p>Description: <strong>{props.description}</strong></p>
         <p>year: <em>{props.year}</em></p>
-        { props.children }
+        <input type="text" onChange={props.onChangeCarTitleHandler} value={props.title}/>
+        <button onClick={props.onDeleteCarHandler}>Delete</button>
     </div>
 )
 
